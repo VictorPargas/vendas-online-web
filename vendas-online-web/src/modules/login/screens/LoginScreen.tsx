0b47da15833d343751f3e2cc92fa993d49899046
Victor Parga <victorparga765@gmail.com>
2024-10-16 22:25:04 -0300

    feat: add image and styeled components

---
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import { BackGroundImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+   return <div>
+    <BackGroundImage src="./BackgroundImage.jpg" />
+   </div>
+}
+
+
+export default LoginScreen;
